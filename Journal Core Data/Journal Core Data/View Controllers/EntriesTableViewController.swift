b75246c2a7312c18d420ec5ea634999760fce0dc
Donovan Langager <donovan.lan16@gmail.com>
2019-09-19 15:46:14 -0700

    Day 3.5 adding firebase

--- Journal Core Data/Journal Core Data/View Controllers/EntriesTableViewController.swift
+++ Journal Core Data/Journal Core Data/View Controllers/EntriesTableViewController.swift
@@ -7,50 +7,67 @@
 //
 
 import UIKit
+import CoreData
 
 class EntriesTableViewController: UITableViewController {
+    
+    let entryController = EntryController()
+    
+    lazy var fetchResultsController: NSFetchedResultsController<Entry> = {
+        
+        let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "priority", ascending: true)]
+        
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.shared.mainContext, sectionNameKeyPath: "Priority", cacheName: nil)
+        
+        frc.delegate = self
+        
+        do {
+            try frc.performFetch()
+        } catch {
+            fatalError("Error performing fetch for frc: \(error)")
+        }
+        return frc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return fetchResultsController.sections?.count ?? 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return fetchResultsController.sections?[section].numberOfObjects ?? 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EntryCell", for: indexPath)
+        
+        let entry = fetchResultsController.object(at: indexPath)
+        
+        cell.textLabel?.text = entry.title
 
         return cell
     }
-    */
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -60,24 +77,19 @@ class EntriesTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
-    /*
+
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
-    */
 
-    /*
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -85,6 +97,5 @@ class EntriesTableViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
--- Journal Core Data/Journal Core Data/View Controllers/EntryController.swift
+++ Journal Core Data/Journal Core Data/View Controllers/EntryController.swift
@@ -7,8 +7,153 @@
 //
 
 import Foundation
+import CoreData
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
 
 class EntryController {
+    
+    // We are using this as the "viewDidLoad" of the TaskController
+    init() {
+        fetchTasksFromServer()
+    }
+    
+    let baseURL = URL(string: "https://tasks-3f211.firebaseio.com/")!
+    
+    func put(entry: Entry, completion: @escaping () -> Void = { }) {
+        
+        let identifier = entry.identifier ?? UUID()
+        entry.identifier = identifier
+        
+        let requestURL = baseURL
+            .appendingPathComponent(identifier.uuidString)
+            .appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.put.rawValue
+        
+        guard let entryRepresentation = entry.entryRepresentation else {
+            NSLog("Entry Representation is nil")
+            completion()
+            return
+        }
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(entryRepresentation)
+        } catch {
+            NSLog("Error encoding task representation: \(error)")
+            completion()
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (_, _, error) in
+            
+            if let error = error {
+                NSLog("Error PUTting task: \(error)")
+                completion()
+                return
+            }
+            
+            completion()
+            }.resume()
+    }
+    
+    func fetchTasksFromServer(completion: @escaping () -> Void = { }) {
+        
+        // appendingPathComponent adds a /
+        // appendingPathExtension add a .
+        
+        let requestURL = baseURL.appendingPathExtension("json")
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            
+            if let error = error {
+                NSLog("Error fetching tasks: \(error)")
+                completion()
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                completion()
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                
+                let entryReprentations = try decoder.decode([String: entryRepresentation].self, from: data).map({ $0.value })
+                
+                self.updateTasks(with: entryReprentations)
+                
+            } catch {
+                NSLog("Error decoding: \(error)")
+            }
+            
+            }.resume()
+    }
+    
+    func updateEntries(with representations: [TaskRepresentation]) {
+        
+        //        var identifiersToFetch: [UUID] = []
+        //
+        //        for representation in representations {
+        //            let identifier = UUID(uuidString: representation.identifier)
+        //
+        //            guard let identifier2 = identifier else { continue }
+        //
+        //            identifiersToFetch.append(identifier2)
+        //        }
+        
+        let identifiersToFetch = representations.compactMap({ UUID(uuidString: $0.identifier) })
+        
+        // [UUID: TaskRepresentation]
+        
+        let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, representations))
+        
+        // Make a mutable copy of the dictionary above
+        var tasksToCreate = representationsByID
+        
+        do {
+            let context = CoreDataStack.shared.mainContext
+            
+            let fetchRequest: NSFetchRequest<Task> = Entry.fetchRequest()
+            
+            // identifier == \(identifier)
+            fetchRequest.predicate = NSPredicate(format: "identifier IN %@", identifiersToFetch)
+            
+            // Which of these tasks exist in Core Data already?
+            let existingTasks = try context.fetch(fetchRequest)
+            
+            // Which need to be updated? Which need to be put into Core Data?
+            for entry in existingEntries {
+                guard let identifier = entry.identifier,
+                    // This gets the task representation that corresponds to the task from Core Data
+                    let representation = representationsByID[identifier] else { continue }
+                
+                entry.name = representation.name
+                entry.notes = representation.notes
+                entry.priority = representation.priority
+                
+                entriesToCreate.removeValue(forKey: identifier)
+            }
+            
+            // Take the tasks that AREN'T in Core Data and create new ones for them.
+            for representation in tasksToCreate.values {
+                Entry(taskRepresentation: representation, context: context)
+            }
+            
+            CoreDataStack.shared.saveToPersistentStore()
+            
+        } catch {
+            NSLog("Error fetching entries from persistent store: \(error)")
+        }
+    }
+    
     
     @discardableResult func createEntry(with title: String, bodyText: String, identifier: String, timestamp: Date) -> Entry {
         
